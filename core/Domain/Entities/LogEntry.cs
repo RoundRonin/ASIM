831d3f4f5993d9bf2f7b5dbf837f74f288257f33
Djurdje Pavlovic <djurdjepavlovic@gmail.com>
2025-05-22 22:15:09 +0300

    Added Entities

---
+++ core/Domain/Entities/LogEntry.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class LogEntry
+    {
+        public int LogId { get; set; }
+        public string Action { get; set; }
+        public DateTime Timestamp { get; set; }
+        public string Description { get; set; }
+
+        public int UserId { get; set; }
+        public User User { get; set; }
+
+        public int ObjectId { get; set; }
+        public InventoryObject InventoryObject { get; set; }
+    }
+}
