fc37e9fa075c973ddfe40f83c1abe3bc633c85a5
Anton <roundronin8@gmail.com>
2025-06-02 22:25:56 +0300

    chore: add minor files

---
+++ back/Infrastructure/Migrations/20250602191129_AutoMigration_.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AutoMigration_ : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "InventoryObjects",
+                columns: table => new
+                {
+                    ObjectId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    SerialNumber = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Location = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Condition = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    QrCode = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_InventoryObjects", x => x.ObjectId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    SenderCity = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    SenderAddress = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    ReceiverCity = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    ReceiverAddress = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    CargoWeight = table.Column<double>(type: "double precision", nullable: false),
+                    PickupDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    OrderNumber = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Email = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    PasswordHash = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: false),
+                    Role = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    LastLogin = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AdditionalObjectDetails",
+                columns: table => new
+                {
+                    DetailsId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ObjectId = table.Column<int>(type: "integer", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    Model = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Manufacturer = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    PurchaseDate = table.Column<DateTime>(type: "date", nullable: false),
+                    Warranty = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    TechnicalSpecifications = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AdditionalObjectDetails", x => x.DetailsId);
+                    table.ForeignKey(
+                        name: "FK_AdditionalObjectDetails_InventoryObjects_ObjectId",
+                        column: x => x.ObjectId,
+                        principalTable: "InventoryObjects",
+                        principalColumn: "ObjectId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BlacklistEntries",
+                columns: table => new
+                {
+                    BlacklistId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UserId = table.Column<int>(type: "integer", nullable: false),
+                    RevokedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Reason = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BlacklistEntries", x => x.BlacklistId);
+                    table.ForeignKey(
+                        name: "FK_BlacklistEntries_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LogEntries",
+                columns: table => new
+                {
+                    LogId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Action = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    Timestamp = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    UserId = table.Column<int>(type: "integer", nullable: false),
+                    ObjectId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LogEntries", x => x.LogId);
+                    table.ForeignKey(
+                        name: "FK_LogEntries_InventoryObjects_ObjectId",
+                        column: x => x.ObjectId,
+                        principalTable: "InventoryObjects",
+                        principalColumn: "ObjectId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_LogEntries_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Rentals",
+                columns: table => new
+                {
+                    RentalId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    StartDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Status = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    ObjectId = table.Column<int>(type: "integer", nullable: false),
+                    RenterId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rentals", x => x.RentalId);
+                    table.ForeignKey(
+                        name: "FK_Rentals_InventoryObjects_ObjectId",
+                        column: x => x.ObjectId,
+                        principalTable: "InventoryObjects",
+                        principalColumn: "ObjectId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Rentals_Users_RenterId",
+                        column: x => x.RenterId,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RepairRequests",
+                columns: table => new
+                {
+                    RequestId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    Status = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    ObjectId = table.Column<int>(type: "integer", nullable: false),
+                    CreatedById = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RepairRequests", x => x.RequestId);
+                    table.ForeignKey(
+                        name: "FK_RepairRequests_InventoryObjects_ObjectId",
+                        column: x => x.ObjectId,
+                        principalTable: "InventoryObjects",
+                        principalColumn: "ObjectId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_RepairRequests_Users_CreatedById",
+                        column: x => x.CreatedById,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AdditionalObjectDetails_ObjectId",
+                table: "AdditionalObjectDetails",
+                column: "ObjectId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BlacklistEntries_UserId",
+                table: "BlacklistEntries",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LogEntries_ObjectId",
+                table: "LogEntries",
+                column: "ObjectId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LogEntries_UserId",
+                table: "LogEntries",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rentals_ObjectId",
+                table: "Rentals",
+                column: "ObjectId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rentals_RenterId",
+                table: "Rentals",
+                column: "RenterId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RepairRequests_CreatedById",
+                table: "RepairRequests",
+                column: "CreatedById");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RepairRequests_ObjectId",
+                table: "RepairRequests",
+                column: "ObjectId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AdditionalObjectDetails");
+
+            migrationBuilder.DropTable(
+                name: "BlacklistEntries");
+
+            migrationBuilder.DropTable(
+                name: "LogEntries");
+
+            migrationBuilder.DropTable(
+                name: "Orders");
+
+            migrationBuilder.DropTable(
+                name: "Rentals");
+
+            migrationBuilder.DropTable(
+                name: "RepairRequests");
+
+            migrationBuilder.DropTable(
+                name: "InventoryObjects");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
